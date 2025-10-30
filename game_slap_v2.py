68bc63347f7bedfafd4122448a1f62e65fd4136f
ersildan <11free11@mail.ru>
2025-10-30 21:47:08 +0300

    +

---
+++ game_slap_v2.py
@@ -0,0 +1,13 @@
+class unit:
+    def __init__(self):
+        self.name = 'unit'
+        self.hp = 5
+    
+class player:
+    def __init__(self):
+        self.name = 'player'
+        self.hp = 5
+
+
+obj = unit()
+print(obj.name)
