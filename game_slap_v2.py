22d77523aaec95c4254cad19ee5d74d1e1a047ff
Andrey Svistunov <120823381+ersildan@users.noreply.github.com>
2025-11-06 00:29:18 +0300

    Update game_slap_v2.py

--- game_slap_v2.py
+++ game_slap_v2.py
@@ -1,7 +1,15 @@
 class Unit:
-
-    @staticmethod
-    def player():
-        name = 'unit'
-        hp = 5
+    def __init__(self, name='Юнит', hp=5):
+        self.name = name
+        self.hp = hp
+    
+    def take_damage(self, damage):
+        self.hp -= damage
+        return self.hp
+    
+    def is_alive(self):
+        return self.hp > 0
+    
+    def get_status(self):
+        return f"{self.name}: {self.hp} здоровья"
     
